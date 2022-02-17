316ca0790aa029102f50fcdf647e0310bfd59d18
German <german.a.almonte@gmail.com>
2022-02-16 19:44:29 -0500

    set up base files

---
+++ weatherapi/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Weather Application</h1>
+      </header>
+        <main>
+
+        </main>
+        <footer>
+          Made by German Almonte
+        </footer>
+    </div>
+  );
+}
+
+export default App;
